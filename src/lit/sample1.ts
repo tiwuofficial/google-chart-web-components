6a8a08ecba99e3fe11e351ec860bf5ccfa1a6468
tiwuofficial <theimaginationwasunbroken@gmail.com>
2022-05-07 11:57:11 +0900

    fetch data

--- src/lit/sample1.ts
+++ src/lit/sample1.ts
@@ -16,8 +16,41 @@ export class SimpleGreeting extends LitElement {
   @property()
   name?: string = 'World';
 
+  @property({attribute: false})
+  hoge: string = 'b';
+
+  async getData() {
+    const response = await fetch(
+      'https://d2gye820k6mno1.cloudfront.net/public/azuki/collections/2022-04-24.json'
+    ).catch(() => {
+      return Response.error();
+    });
+    if (!response.ok) {
+      return undefined;
+    }
+    const data = await response
+      .json()
+      .catch(() => {
+        return undefined;
+      });
+    if (!data) {
+      return undefined;
+    }
+    return data;
+  }
+
+  constructor() {
+    super();
+    console.log('a');
+    console.log(this.getData().then(data => {
+      console.log(data);
+      this.hoge = 'c';
+    }));
+  }
+
   render() {
     return html`
+      <p>${this.hoge}</p>
       <google-chart
         data='[["day", "${this.name}"],["1/1", 0],["1/2", 10],["1/3", 23],["1/4", 17],["1/5", 18],["1/6", 9]]'
         type='line'
